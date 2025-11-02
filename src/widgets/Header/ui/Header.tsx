91bd0e41c18e641a44b2334442ce75cbda659b93
Oleh <berolegnik@gmail.com>
2025-11-02 18:47:24 +0200

    добавил шрифты

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -4,31 +4,33 @@ import { FiSearch, FiBell, FiCalendar } from "react-icons/fi";
 export const Header = () => {
   return (
     <header className="dashboard__header">
-      <div className="dashboard__header-logo">
-        <span className="dashboard__header-logo-highlight">Dash</span>board
-      </div>
+      <div className="dashboard__header-container">
+        <div className="dashboard__header-logo">
+          <span className="dashboard__header-logo-highlight">Dash</span>board
+        </div>
 
-      <div className="dashboard__header-search">
-        <input
-          type="text"
-          className="dashboard__header-input"
-          placeholder="Search your task here..."
-        />
-        <button className="dashboard__header-button dashboard__header-button-search">
-          <FiSearch />
-        </button>
-      </div>
+        <div className="dashboard__header-search">
+          <input
+            type="text"
+            className="dashboard__header-input"
+            placeholder="Search your task here..."
+          />
+          <button className="dashboard__header-button dashboard__header-button-search">
+            <FiSearch />
+          </button>
+        </div>
 
-      <div className="dashboard__header-actions">
-        <button className="dashboard__header-button dashboard__header-button-bell">
-          <FiBell />
-        </button>
-        <button className="dashboard__header-button dashboard__header-button-calendar">
-          <FiCalendar />
-        </button>
-        <div className="dashboard__header-date">
-          <span className="dashboard__header-day">Tuesday</span>
-          <span className="dashboard__header-full-date">20/06/2023</span>
+        <div className="dashboard__header-actions">
+          <button className="dashboard__header-button dashboard__header-button-bell">
+            <FiBell />
+          </button>
+          <button className="dashboard__header-button dashboard__header-button-calendar">
+            <FiCalendar />
+          </button>
+          <div className="dashboard__header-date">
+            <span className="dashboard__header-day">Tuesday</span>
+            <span className="dashboard__header-full-date">20/06/2023</span>
+          </div>
         </div>
       </div>
     </header>
