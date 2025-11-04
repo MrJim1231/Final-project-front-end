2d970b85df94418facec66b08a021a38b9e59b02
Oleh <berolegnik@gmail.com>
2025-11-04 18:44:43 +0200

    добавил Setting

--- src/pages/settings/ui/Settings.tsx
+++ src/pages/settings/ui/Settings.tsx
@@ -1,10 +1,67 @@
 import "./Settings.css";
+import userAvatar from "../../../shared/assets/images/avatar.png"; // ⚙️ замени на свой путь
 
 export const Settings = () => {
   return (
-    <section className="page">
-      <h2 className="page__title">Settings</h2>
-      <p className="page__text">Change your preferences and account details.</p>
+    <section className="settings">
+      <div className="settings__header">
+        <h3 className="settings__title">Account Information</h3>
+        <button className="settings__back">Go Back</button>
+      </div>
+
+      <div className="settings__content">
+        {/* === User info === */}
+        <div className="settings__user">
+          <img src={userAvatar} alt="User" className="settings__avatar" />
+          <div className="settings__user-info">
+            <h4 className="settings__user-name">Sundar Gurung</h4>
+            <p className="settings__user-email">sundargurung360@gmail.com</p>
+          </div>
+        </div>
+
+        {/* === Form === */}
+        <form className="settings__form">
+          <label className="settings__label">
+            First Name
+            <input type="text" className="settings__input" />
+          </label>
+
+          <label className="settings__label">
+            Last Name
+            <input type="text" className="settings__input" />
+          </label>
+
+          <label className="settings__label">
+            Email Address
+            <input type="email" className="settings__input" />
+          </label>
+
+          <label className="settings__label">
+            Contact Number
+            <input type="tel" className="settings__input" />
+          </label>
+
+          <label className="settings__label">
+            Position
+            <input type="text" className="settings__input" />
+          </label>
+
+          <div className="settings__actions">
+            <button
+              type="submit"
+              className="settings__btn settings__btn--update"
+            >
+              Update Info
+            </button>
+            <button
+              type="button"
+              className="settings__btn settings__btn--password"
+            >
+              Change Password
+            </button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 };
