2d83fb487d06f901e8efb8323281a6f720265070
Oleh <berolegnik@gmail.com>
2025-11-07 23:26:17 +0200

    redux

---
+++ src/entities/task/model/selectors.ts
@@ -0,0 +1,6 @@
+// src/entities/task/model/selectors.ts
+import { RootState } from "@/app/providers/store";
+
+export const selectTasks = (state: RootState) => state.tasks.items;
+export const selectTasksLoading = (state: RootState) => state.tasks.loading;
+export const selectSelectedTask = (state: RootState) => state.tasks.selected;
